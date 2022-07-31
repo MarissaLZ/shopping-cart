abdc894f57fe58e739ae9007333f67f65a43e08a
Tim <tim.almacen@gmail.com>
2022-07-30 22:36:48 -0700

    new Product type created function getData that handles fetch method using generic type response

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -2,69 +2,43 @@ import storeItems from "../data/items.json"
 import { Col, Row } from "react-bootstrap"
 import { StoreItem } from '../components/StoreItem'
 import React, { useEffect, useState } from 'react'
-import {getData} from "./storeAPI"
+// import {getData} from "./storeAPI"
 
-
-// type StoreProps = {
-//   getData: void
-//   // url: string
-// }
-
-interface Products {
-  id?: number
-  title?: string
-  price?: number
-  description?: string
-  category?: string
-  image?: string
-  rating: object
+//Product type interface to handle fetched data
+interface Product {
+  category: string
+  description: string
+  id: number
+  image: string
+  price: number
+  rating?: object
+  title: string
 }
+
+// function getData<T>(url: string): Promise<T> {
+//   return fetch(url).then(response => {
+//     return response.json<T>()
+//   })
+// }
 
 // export function Store({getData}: StoreProps)  {
 export function Store()  {
+  const [product, setProduct] = useState([])
+  const url: string = "https://fakestoreapi.com/products/";
 
-  const [productList, setProductList] = useState <any>([])
+  function getData<T>(url): Promise<T> {
+    return fetch(url)
+    .then(response => {
+      return response.json<T>()
+    })
+    .then()
+  }
+  getData<Product>(url) 
+  .then((item) => {{
+    let fetchedData: Product = item;
+    console.log('generic data!',fetchedData)
+  }})
 
-//   function getUsers(): Promise<User[]> {
-//     // For now, consider the data is stored on a static `users.json` file
-//     return fetch('/users.json')
-//             // the JSON body is taken from the response
-//             .then(res => res.json())
-//             .then(res => {
-//                     // The response has an `any` type, so we need to cast
-//                     // it to the `User` type, and return it from the promise
-//                     return res as User[]
-//             })
-// }
-const url2 = "https://fakestoreapi.com/products/";
-
-  // function getDataAgain(url3): Promise<Products> {
-  //   return fetch(url3)
-  //   .then((response) => response.json())
-  //   .then(response => {
-  //     return response as Products[]
-  //   })
-  //  }
-  //  getDataAgain()
-
-//   export function getData(url: string) {
-
-//     fetch(url)
-//     .then((response) => response.json())
-
-// }
-  useEffect(() => {
-    //const url = 'https://fakestoreapi.com/products/'
-    const url2 = "https://fakestoreapi.com/products/";
-    getData(url2)
-    .then(res => setProductList(res))
-
-  }, [])
-
-  console.log('product list after',productList)
-  // console.log('response', getData())
-  // getData()
-  //storeItems is an array of objects
     return(
       <>
         <h1>Store</h1>
